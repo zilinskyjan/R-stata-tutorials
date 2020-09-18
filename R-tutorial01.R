ac5f0a4617bcae7409214c1b57ae10ea072ab27f
Jan Zilinsky <zilinskyjan@users.noreply.github.com>
2020-09-18 03:02:05 -0400

    finalize tidyverse lesson

--- R-tutorial01.R
+++ R-tutorial01.R
@@ -44,6 +44,16 @@ CD %>% summarise(number_of_districts = n())
 
 CD %>% tally()
 
+
+
+# Let's list the KEY VERBS
+# filter: Keep only some rows (depending on their particular values).
+# select: Keep the specified columns (list their names, without quotation marks).
+# mutate: Create new variables.
+# summarise: Collapse multiple rows into a single summary value.
+# arrange: Order rows based on their values.
+
+
 # Calculate the average Clinton vote share
 CD %>% summarise(avg_HRC_vote_share = mean(Clinton16))
 
@@ -62,20 +72,60 @@ CD %>% group_by(Cluster) %>%
   arrange(avg_HRC_vote_share)
 
 # Sort your data from highest to lowest average Clinton vote share
+# and show the total number of districts per row:
 CD %>% group_by(Cluster) %>%
-  summarise(avg_HRC_vote_share = mean(Clinton16)) %>%
+  summarise(avg_HRC_vote_share = mean(Clinton16),
+            n=n()) %>%
   arrange(-avg_HRC_vote_share)
 
 ##########################
 # GENERATING NEW VARIABLES (as a function of what we already have)
 ##########################
-# Task: How did HRC do relative to the national average
+# Create a binary variable conveying the district is "safe Democratic"
+CD %>% mutate(Clinton16_over70 = ifelse(Clinton16 >= .7,1,0))
+
+CD <- CD %>% mutate(Clinton16_over70 = ifelse(Clinton16 >= .7,1,0))
+
+# Check 3 randomly selected district from each group:
+CD %>% group_by(Clinton16_over70) %>% 
+  sample_n(3) %>% 
+  select(CD,Clinton16_over70,Clinton16)
+
+# How many such (arbitrarily defined) safe districts are there?
+CD %>% count(Clinton16_over70)
+
+# What is the typical density in these types of districts?
+CD %>% filter(Clinton16_over70==1) %>%
+  count(Cluster) %>%
+  arrange(-n)
+
+
+
+# NEW TASK: How did HRC do relative to the national average
 CD %>% mutate(avg_HRC_vote_share = mean(Clinton16))
 
 # To see what happened, run:
 CD %>% mutate(avg_HRC_vote_share = mean(Clinton16)) %>%
-  select(1,2,Clinton16,26)
+  select(1,2,Clinton16,avg_HRC_vote_share)
 
+# Calculate relative performance
+CD %>% mutate(avg_HRC_vote_share_national = mean(Clinton16)) %>%
+  select(1,2,Clinton16,avg_HRC_vote_share_national)
+
+# What else do we need?
+CD %>% mutate(avg_HRC_vote_share_national = mean(Clinton16)) %>%
+  select(1,2,Clinton16,avg_HRC_vote_share_national) %>%
+  mutate(relative_performance = Clinton16 - avg_HRC_vote_share_national) 
+
+(plot1 <- CD %>% mutate(avg_HRC_vote_share_national = mean(Clinton16)) %>%
+  select(1,2,Clinton16,avg_HRC_vote_share_national) %>%
+  mutate(relative_performance = Clinton16 - avg_HRC_vote_share_national) %>% 
+  select(relative_performance) %>%
+  ggplot(aes(x=relative_performance)) +
+  geom_histogram(colour = '#CCCCCC',fill = '#3173B0',size = .5,linetype = 1,alpha = 0.6) )
+
+# library('ggedit')
+# ggedit(plot1)
 
 # Inspect individual rows and cells
 ###############################################
@@ -106,4 +156,29 @@ CD %>% filter(CD=="NY-14") %>% dplyr::select(Clinton16)
 CD %>% filter(CD=="NY-14") %>% dplyr::select(Clinton16)
 
 
+# Looking at state-level observations
+library(stringr)
+CD %>% filter(str_detect(CD,"NY"))
 
+CD %>% filter(grepl("NY",CD))
+
+# Usually a better choice: generate a new variable
+substr(CD$CD,1,2) 
+
+CD$state <- substr(CD$CD,1,2) 
+
+View(CD)
+
+# Average performance by state?
+CD %>% group_by(state) %>% summarise(HRC = mean(Clinton16),
+                                     DJT = mean(Trump16))
+# Typical performance by state?
+CD %>% group_by(state) %>% summarise(HRC = median(Clinton16),
+                                     DJT = median(Trump16))
+
+# Typical performance by state and by district type
+CD %>% group_by(state,Cluster) %>% summarise(HRC = median(Clinton16),
+                                     DJT = median(Trump16),
+                                     n = n())
+
+# Number of Democratic incumbents by state?
